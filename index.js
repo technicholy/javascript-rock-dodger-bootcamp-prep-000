ef0edd5f34897aa3d832667c7a63e6cdb4b2016a
Clayton Pierce <bamer78@gmail.com>
2020-08-23 04:09:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -120,8 +120,12 @@ function endGame() {
 
 function moveDodger(e) {
   if (e.which === LEFT_ARROW){
+    e.preventDefault()
+    e.stopPropagation()
     moveDodgerLeft()
   } else if (e.which === RIGHT_ARROW){
+    e.preventDefault()
+    e.stopPropagation()
     moveDodgerRight()
   }
 }
